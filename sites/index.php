90c9052183ae995f09bc3eb7faa9dd96da49e70f
Heiko Göhler <heiko.goehler@k-tc.de>
2016-11-29 16:41:23 +0100

    Added sort of a structure

---
+++ sites/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!--
+Best License EU
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        ?>
+    </body>
+</html>
