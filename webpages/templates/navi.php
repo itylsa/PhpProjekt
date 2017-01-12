a756433c29b96e24603244669120dfd364f3cf46
joerg.gulde <joerg.gulde@WSDEKA6.ktc.local>
2017-01-12 10:08:57 +0100

    new php page for user edit and other random shit

--- webpages/templates/navi.php
+++ webpages/templates/navi.php
@@ -1,9 +1,9 @@
 <h3>Navigation</h3>
 <nav>
   <ul>
+    <li><a href="userEditView.php">Account</a></li> 
     <li><a href="createAnnonce.php">Annonce erstellen</a></li>
     <li><a href="overview.php">Übersicht</a></li>
-    <li><a href="user.php">Account</a></li> 
     <li><a href="logout.php">Logout</a></li>
   </ul>
 </nav>
