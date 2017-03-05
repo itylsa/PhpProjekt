4df458b6ea325182d4811d28a0442956d08eea5f
RaerS <sandro.ivanisevic@outlook.com>
2017-03-05 15:56:34 +0100

    Update anzeige.php

--- anzeige.php
+++ anzeige.php
@@ -9,22 +9,13 @@ $id = $_GET['id'];
 
 if ($_POST['beobachten']){
 	$caid = $_POST['caid'];
-	$cuid = $_POST['cuid'];
+	$uaid = $_POST['uaid'];
 	$datum = date("d.m.Y");
-	$check = mysqli_query($connection, "SELECT count(bid) AS anzahl FROM `beobachtungen` WHERE `aid`='$caid' AND `uid`='$cuid'");
+	$check = mysqli_query($connection, "SELECT count(bid) AS anzahl FROM `beobachtungen` WHERE `aid`='$caid' AND `uid`='$uaid'");
 	$check2 = mysqli_fetch_assoc($check);
 	if ($check2['anzahl'] == "0"){
-		mysqli_query($connection, "INSERT INTO `beobachtungen` (`aid`, `uid`, `datum`) VALUES ('$caid', '$cuid', '$datum')");
+		mysqli_query($connection, "INSERT INTO `beobachtungen` (`aid`, `uid`, `datum`) VALUES ('$caid', '$uid', '$datum')");
 	}
-}
-
-if ($_POST['kontaktieren']){
-	$caid = $_POST['caid'];
-	$cuid = $_POST['cuid'];
-	$uaid = $_POST['uaid'];
-	$vname = $_POST['vname'];
-	mysqli_query($connection, "INSERT INTO `chat` (`vid`, `vname`, `kid`, `kname`) VALUES ('$uaid', '$vname', '$cuid', '$vorname')");
-	header("Location: http://dkws.goip.de/schule/chat.php");
 }
 
 ?>
@@ -32,8 +23,8 @@ if ($_POST['kontaktieren']){
 <!DOCTYPE html>
 <html lang="de">
 <head>
-<title>Car&Bike Kleinanzeigen</title>
-<link rel="icon" href="motorcycle-side-view.png">
+<title>Kleinanzeigen</title>
+
 <link rel="stylesheet" type="text/css" href="./style.css">
 </head>
 
@@ -43,7 +34,7 @@ if ($_POST['kontaktieren']){
   <div id="kopfbereich">
     <center>
 			<br>
-			<a href="index.php"><img src="./carandbike.png" width="900px"/></a>
+			<a href="index.php" style="text-decoration: none; color: black;"><h1>Kleinanzeigen</h1></a>
 			<br>
        <br>
     </center>
@@ -113,13 +104,6 @@ if ($_POST['kontaktieren']){
 										$kontakt = $ad['name'];
 										$titel = $ad['titel'];
 										$kategorie = $ad['kategorie'];
-										$kilometer = $ad['kilometer'];
-										$ezmonat = $ad['ezmonat'];
-										$ezjahr = $ad['ezjahr'];
-										$vorbesitzer = $ad['vorbesitzer'];
-										$leistung = $ad['leistung'];
-										$getriebe = $ad['getriebe'];
-										$tueren = $ad['tueren'];
 										$preistyp = $ad['preistyp'];
 										$preis = $ad['preis'];
 										$plz = $ad['plz'];
@@ -135,7 +119,7 @@ if ($_POST['kontaktieren']){
 										$resultortsabfrage = mysqli_fetch_assoc($ortsabfrage);
 										$adort = $resultortsabfrage['ort'];
 							
-										if($kategorie == "Auto"){
+
 												echo "<br>
 															<table width='80%'>
 																<tr>
@@ -150,18 +134,11 @@ if ($_POST['kontaktieren']){
 																	<td style='vertical-align: top; text-align: right;'><br>Erstellt von $kontakt<br>Beobachter: $beobachter";
 																	
 																	if ($uaid != $uid){
-																		echo "<br><br><form action='' method='post'><input type='hidden' name='caid' value='$id'/><input type='hidden' name='cuid' value='$uid' /><input type='hidden' name='uaid' value='$uaid' /><input type='hidden' name='vname' value='$kontakt' /><input type='submit' name='beobachten' value='Jetzt beobachten'/><br><input type='submit' name='kontaktieren' value='Verkäufer kontaktieren'/></form>";
+																		echo "<br><br><form action='' method='post'><input type='hidden' name='caid' value='$aid' /><input type='hidden' name='uaid' value='$uaid' /><input type='submit' name='beobachten' value='Jetzt beobachten'/></form>";
 																	}
 												echo "</td>
 														</tr>
 															</table>
-															<br><br>
-															<table width='50%'>
-																<tr>
-																	<td><b><p>Kilometer</p><p>Erstzulassung</p><p>Leistung in PS</p><p>Getriebe</p><p>Türen</p><p>Vorbesitzer</p></b></td>
-																	<td><p>$kilometer</p><p>$ezmonat/$ezjahr</p><p>$leistung</p><p>$getriebe</p><p>$tueren</p><p>$vorbesitzer</p></td>
-																</tr>
-															</table>
 															<br>
 															<table width='70%' cellspacing='10'>
 																<tr>
@@ -183,54 +160,6 @@ if ($_POST['kontaktieren']){
 															<p>Erstellt am $datum</p>
 															<br>
 															<hr>";
-										} else {
-											echo "<br>
-															<table width='80%'>
-																<tr>
-																	<td align='left' style='font-weight: bold;'><h2>$titel</h2></td>
-																</tr>
-																</tr>
-																	<td width='120px'></td>
-																	<td align='right' width='150px' style='font-weight: bold;'>$preis € $preistyp</td>
-																</tr>
-																<tr>
-																	<td><img src='adphotos/$aid/1.jpg' width='400px'/><p>$plz $adort</p></td>
-																	<td style='vertical-align: top; text-align: right;'><br>Erstellt von $kontakt<br>Beobachter: $beobachter";
-																	
-																	if ($uaid != $uid){
-																		echo "<br><br><form action='' method='post'><input type='hidden' name='caid' value='$id'/><input type='hidden' name='cuid' value='$uid' /><input type='hidden' name='uaid' value='$uaid' /><input type='submit' name='beobachten' value='Jetzt beobachten'/><br><input type='submit' name='kontaktieren' value='Verkäufer kontaktieren'/></form>";
-																	}
-														echo "</td></tr>
-															</table>
-															<br><br>
-															<table width='50%'>
-																<tr>
-																	<td><b><p>Kilometer</p><p>Erstzulassung</p><p>Leistung in PS</p><p>Getriebe</p><p>Vorbesitzer</p></b></td>
-																	<td><p>$kilometer</p><p>$ezmonat/$ezjahr</p><p>$leistung</p><p>$getriebe</p><p>$vorbesitzer</p></td>
-																</tr>
-															</table>
-															<br>
-															<table width='70%' cellspacing='10'>
-																<tr>
-																	<td style='font-weight: bold;'>Beschreibung</td>
-																</tr>
-																<tr>
-																	<td><hr></td>
-																</tr>
-																<tr>
-																	<td>";
-																				echo nl2br($beschreibung);
-												echo "</td>
-																</tr>
-																<tr>
-																	<td><hr></td>
-																</tr>
-															</table>
-															<br>
-															<p>Erstellt am $datum</p>
-															<br>
-															<hr>";
-										}
 									}
 			?>
 		
@@ -243,7 +172,7 @@ if ($_POST['kontaktieren']){
 <footer>
   <div id="fussbereich">
     <br>
-		<p>Copyright &copy; <?php $copyrightyear = date("Y"); echo "$copyrightyear";?> Car&Bike Kleinanzeigen. Alle Rechte vorbehalten.<p>
+		<p>Copyright &copy; <?php $copyrightyear = date("Y"); echo "$copyrightyear";?> Kleinanzeigen. Alle Rechte vorbehalten.<p>
 		<br>
   </div>
 </footer>
